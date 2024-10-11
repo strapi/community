90c7ba0183bf7b4dec5a47ab4ad0fcb0b2c50478
Aurélien Georget <aurelsicoko@gmail.com>
2024-10-11 10:55:05 +0200

    Homepage UI in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,82 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/app/css/layout.module.css';
+
+import { Checkbox, Grid, Flex, Typography } from '@strapi/design-system';
+import { ActionCard } from '@/app/ui/actionCard';
+
+export const metadata = {
+  title: 'Strapi Market',
+  description:
+    'Power up your Strapi app with the industry-leading software and easily add custom features using plugins.',
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <Grid.Item col={3} direction={'column'} alignItems={'flex-start'}>
+        <Flex
+          className={styles.filters}
+          direction={'column'}
+          alignItems={'flex-start'}
+          gap={'8px'}
+        >
+          <Checkbox>Plugins (x,xxx)</Checkbox>
+          <Checkbox>Providers (yyy)</Checkbox>
+        </Flex>
+        <Flex
+          className={styles.categories}
+          direction={'column'}
+          alignItems={'flex-start'}
+          gap={'4px'}
+        >
+          <Link className={styles.category} href='/categories/analytics'>
+            Analytics
+          </Link>
+          <Link className={styles.category} href='/categories/authentication'>
+            Authentication
+          </Link>
+          <Link className={styles.category} href='/categories/commerce'>
+            Commerce
+          </Link>
+          <Link className={styles.category} href='/categories/databases'>
+            Databases
+          </Link>
+          <Link className={styles.category} href='/categories/logging'>
+            Logging
+          </Link>
+          <Link className={styles.category} href='/categories/monitoring'>
+            Monitoring
+          </Link>
+          <Link className={styles.category} href='/categories/searching'>
+            Searching
+          </Link>
+          <Link className={styles.category} href='/categories/security'>
+            Security
+          </Link>
+          <Link className={styles.category} href='/categories/testing'>
+            Testing
+          </Link>
+        </Flex>
+        <Flex
+          className={styles.actionCards}
+          direction={'column'}
+          alignItems={'flex-start'}
+          gap={'24px'}
+        >
+          <ActionCard
+            link='https://feedback.strapi.io/plugin-requests'
+            title='Missing a plugin?'
+            color='purple'
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            Tell us what plugin you are looking for and we'll let our community
+            plugin developers know in case they are in search for inspiration!
+          </ActionCard>
+          <ActionCard title='Contribute' color='green'>
+            Develop your own plugin and submit it to the marketplace!
+          </ActionCard>
+        </Flex>
+      </Grid.Item>
+    </>
   );
 }
