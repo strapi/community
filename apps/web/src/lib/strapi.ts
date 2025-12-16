5e1b35d29e7a59660a5f3f5d60e2861dc8303980
Boaz Poolman <boaz_skater@hotmail.com>
2025-12-16 18:03:01 +0100

    feat: add a type safe wrapper around the @strapi/client package

--- apps/web/src/lib/strapi.ts
+++ apps/web/src/lib/strapi.ts
@@ -1,6 +1,6 @@
-import { strapi } from "@strapi/client";
+import { createTypedStrapiClient } from "@repo/strapi-client";
 
-export const client = strapi({
+export const client: ReturnType<typeof createTypedStrapiClient> = createTypedStrapiClient({
   baseURL: process.env.CMS_BASE_URL || 'http://localhost:1337/api',
   auth: process.env.CMS_BEARER_TOKEN,
 });
---
+++ packages/strapi-client/lib/client.ts
@@ -0,0 +1,96 @@
+import { strapi, type API, type Config } from '@strapi/client';
+import type { UID, Modules } from '@strapi/types';
+import pluralize from 'pluralize';
+
+export interface Document {
+  documentId: string;
+  [key: string]: any;
+}
+
+export interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
+
+export interface ResponseMeta {
+  pagination?: Pagination;
+}
+
+export interface DocumentResponse<T extends Document = Document> {
+  data: T;
+  meta: ResponseMeta;
+}
+
+export interface DocumentResponseCollection<T extends Document = Document> {
+  data: T[];
+  meta: ResponseMeta;
+}
+
+// Strapi client does not export thoses types for now:  { ClientCollectionOptions, SingleCollectionOptions } 
+type CollectionOptions = {
+  path: string;
+}
+
+type SingleCollectionOptions = CollectionOptions;
+type ClientCollectionOptions = CollectionOptions;
+
+type GetQueryParams<TContentTypeUID extends UID.ContentType = UID.ContentType> = Modules.Documents.Params.Pick<
+  TContentTypeUID,
+  keyof API.BaseQueryParams
+>;
+
+export type GetAttributes<TContentTypeUID extends UID.ContentType> = Modules.Documents.GetValues<TContentTypeUID> & API.Document;
+
+export const createTypedStrapiClient = (config: Config) => {
+  const client = strapi(config);
+
+  return {
+    fetch: client.fetch.bind(client),
+    baseURL: client.baseURL,
+    files: client.files,
+
+    single: <TContentTypeUID extends UID.ContentType>(resource: TContentTypeUID, options?: SingleCollectionOptions) => {
+      const [, resourceInfo] = resource.split('::') as [string, string];
+      const [, contentTypeName] = resourceInfo.split('.') as [string, string];
+      const clientCollection = client.single(contentTypeName, options);
+
+      return {
+        find: <const TParams extends GetQueryParams<TContentTypeUID>>(queryParams?: TParams) => {
+          return clientCollection.find(queryParams as API.BaseQueryParams) as Promise<DocumentResponse<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        update: <const TParams extends GetQueryParams<TContentTypeUID>>(document: Partial<GetAttributes<TContentTypeUID>>, queryParams?: TParams) => {
+          return clientCollection.update(document, queryParams as API.BaseQueryParams) as Promise<DocumentResponse<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        delete: <const TParams extends GetQueryParams<TContentTypeUID>>(queryParams?: TParams): Promise<void> => {
+          return clientCollection.delete(queryParams as API.BaseQueryParams)
+        },
+      }
+    },
+
+    collection: <TContentTypeUID extends UID.ContentType>(resource: TContentTypeUID, options?: ClientCollectionOptions) => {
+      const [, resourceInfo] = resource.split('::') as [string, string];
+      const [, contentTypeName] = resourceInfo.split('.') as [string, string];
+      const clientCollection = client.collection(pluralize(contentTypeName), options);
+
+      return {
+        find: <const TParams extends GetQueryParams<TContentTypeUID>>(queryParams?: TParams) => {
+          return clientCollection.find(queryParams as API.BaseQueryParams) as Promise<DocumentResponseCollection<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        findOne: <const TParams extends GetQueryParams<TContentTypeUID>>(documentID: string, queryParams?: TParams) => {
+          return clientCollection.findOne(documentID, queryParams as API.BaseQueryParams) as Promise<DocumentResponse<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        create: <const TParams extends GetQueryParams<TContentTypeUID>>(document: Partial<GetAttributes<TContentTypeUID>>, queryParams?: TParams) => {
+          return clientCollection.create(document, queryParams as API.BaseQueryParams) as Promise<DocumentResponse<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        update: <const TParams extends GetQueryParams<TContentTypeUID>>(documentID: string, document: Partial<GetAttributes<TContentTypeUID>>, queryParams?: TParams) => {
+          return clientCollection.update(documentID, document, queryParams as API.BaseQueryParams) as Promise<DocumentResponse<Modules.Documents.Result<TContentTypeUID, TParams>>>;
+        },
+        delete: <const TParams extends GetQueryParams<TContentTypeUID>>(documentID: string, queryParams?: TParams): Promise<void> => {
+          return clientCollection.delete(documentID, queryParams as API.BaseQueryParams)
+        }
+      };
+    }
+  };
+}
