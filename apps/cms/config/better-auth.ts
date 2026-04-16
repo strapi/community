ed92866a0ba7e34b5b81066a4cebdac6b59b821d
Boaz Poolman <boaz_skater@hotmail.com>
2026-04-16 16:34:22 +0200

    fix: don't initiate better auth during build

--- apps/cms/config/better-auth.ts
+++ apps/cms/config/better-auth.ts
@@ -3,8 +3,13 @@ import { strapiAdapter } from "@strapi-community/plugin-better-auth/adapter";
 import { betterAuth } from "better-auth";
 import { organization } from "better-auth/plugins";
 
-const auth = () =>
-  betterAuth({
+const auth = () => {
+  // Skip Better Auth during the build process to avoid issues with missing environment variables
+  if (process.env.npm_lifecycle_script === "strapi build") {
+    return null;
+  }
+
+  return betterAuth({
     trustedOrigins: [process.env.WEBSITE_URL],
     secret: process.env.BETTER_AUTH_SECRET,
     plugins: [organization()],
@@ -20,5 +25,6 @@ const auth = () =>
       },
     },
   });
+};
 
 export default auth;
