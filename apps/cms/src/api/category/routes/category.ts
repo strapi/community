bbebdeffbe6f079b8237f5b9df6f45fc152e6563
Boaz Poolman <boaz_skater@hotmail.com>
2026-04-29 11:24:40 +0200

    fix: add back category content type to fix the deployment

---
+++ apps/cms/src/api/category/routes/category.ts
@@ -0,0 +1,7 @@
+/**
+ * package-category router
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreRouter("api::category.category");
---
+++ apps/cms/src/api/category/services/category.ts
@@ -0,0 +1,7 @@
+/**
+ * package-category service
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreService("api::category.category");
