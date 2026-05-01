aa7810d6ff67aa08b1078070aa13ce44384466da
Boaz Poolman <boaz_skater@hotmail.com>
2026-05-01 21:38:09 +0200

    chore: re-enabled all migrations

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -26,10 +26,10 @@ export default {
       return;
     }
 
-    // await migratePartners();
-    // await migrateIntegrations();
-    // await migratePlugins();
-    // await migrateProviders();
+    await migratePartners();
+    await migrateIntegrations();
+    await migratePlugins();
+    await migrateProviders();
     await migrateShowcases();
   },
 };
