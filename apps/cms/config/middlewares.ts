d564e1c90191d63db937bc3c6fae07ce2cd4f577
Boaz Poolman <boaz_skater@hotmail.com>
2026-06-21 12:35:57 +0200

    chore: fix sso provider logo

--- apps/cms/config/middlewares.ts
+++ apps/cms/config/middlewares.ts
@@ -1,7 +1,32 @@
 export default [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "cdn2.iconfinder.com", // Base URL of the provider's logo
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "cdn2.iconfinder.com", // Base URL of the provider's logo
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
